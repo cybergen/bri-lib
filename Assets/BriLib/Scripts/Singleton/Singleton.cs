29a4c294fb2ebee38bd7fd993cbb3ca46b47a4ce
brian <cybergen@gmail.com>
2017-10-05 05:51:42 -0700

    Added GOSingleton for gameobjects that should initialize once. Added basic Singleton for code-only instantiation. Added ease wrapper for simplified easing behavior (and reversibility). Added MainThreadQueue for queuing an action on the main thread.

--- Assets/BriLib/Scripts/Singleton/Singleton.cs
+++ Assets/BriLib/Scripts/Singleton/Singleton.cs
@@ -1,40 +1,47 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace BriLib
 {
-    public abstract class Singleton<T> : MonoBehaviour, ISingleton where T : UnityEngine.Component, ISingleton
+  /// <summary>
+  /// A singleton that you can easily initialize direct from code. Does not expect any existing game object in scene to initialize it,
+  /// nor any serialized/initiailized fields set by editor
+  /// </summary>
+  /// <typeparam name="T"></typeparam>
+  public abstract class Singleton<T> : MonoBehaviour, ISingleton where T : Component, ISingleton
+  {
+    public static T Instance
     {
-        public static T Instance
+      get
+      {
+        if (_instance == null)
         {
-            get
-            {
-                if (_instance == null)
-                {
-                    var obj = new GameObject(typeof(T).GetType().ToString());
-                    obj.AddComponent<T>();
-                    _instance = obj.GetComponent<T>();
-                    _instance.OnCreate();
-                }
-                return _instance;
-            }
-            private set
-            {
-                _instance = value;
-            }
+          var obj = new GameObject(typeof(T).GetType().ToString());
+          obj.AddComponent<T>();
+          _instance = obj.GetComponent<T>();
+          _instance.OnCreate();
+          _instance.Begin();
         }
-
-        private static T _instance;
-
-        private void Awake()
-        {
-            Instance = gameObject.GetComponent<T>();
-            Instance.OnCreate();
-        }
-
-        public abstract void OnCreate();
-
-        public abstract void Begin();
-
-        public abstract void End();
+        return _instance;
+      }
+      private set
+      {
+        _instance = value;
+      }
     }
+
+    private static T _instance;
+
+    public virtual void OnCreate()
+    {
+      DontDestroyOnLoad(gameObject);
+    }
+
+    public virtual void Begin() { }
+
+    public virtual void End()
+    {
+      Instance = null;
+      Destroy(gameObject);
+    }
+  }
 }
