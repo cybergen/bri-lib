03428bc8c3c169bbfdec36f07ac36d483e7082f0
brian <cybergen@gmail.com>
2017-10-05 20:05:05 -0400

    Deleted UnityTestTools cause they don't... work... anymore? And added some code around EaseWrapper, but it's utterly overhsadowed by the deletions...

--- Assets/BriLib/Scripts/UI/Fadeable.cs
+++ Assets/BriLib/Scripts/UI/Fadeable.cs
@@ -1,18 +1,37 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
-public class Fadeable : MonoBehaviour
+namespace BriLib
 {
-
-  // Use this for initialization
-  void Start()
+  public class Fadeable : MonoBehaviour
   {
+    public CanvasGroup CG;
+    public float StartValue = 0f;
+    public float EndValue = 1f;
+    public float EaseDuration = 0.4f;
+    public Easing.Method EaseType = Easing.Method.ExpoOut;
 
-  }
+    private EaseWrapper easer;
+    private Action<float> _onUpdate;
 
-  // Update is called once per frame
-  void Update()
-  {
+    private void Awake()
+    {
+      easer = new EaseWrapper(EaseDuration, StartValue, EndValue, EaseType, (a) => CG.alpha = a);
+    }
 
+    public void Show(Action onFinish = null, Action onCancel = null)
+    {
+      easer.SetEase(EaseWrapper.Direction.Forward, onFinish, onCancel);
+    }
+
+    public void Hide(Action onFinish = null, Action onCancel = null)
+    {
+      easer.SetEase(EaseWrapper.Direction.Backward, onFinish, onCancel);
+    }
+
+    private void Update()
+    {
+      easer.Tick(Time.deltaTime);
+    }
   }
 }
